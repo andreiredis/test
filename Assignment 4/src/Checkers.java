be8b3574db8ce9e9e62498738dd154f75aaca7a1
andreiredis <andreiredis@gmail.com>
2018-02-27 11:19:53 +0100

    delete checkers print

--- Assignment 4/src/Checkers.java
+++ Assignment 4/src/Checkers.java
@@ -2,36 +2,6 @@ import java.util.Scanner;
 
 public class Checkers {
 	public Scanner s = new Scanner(System.in);
-
-	public static void tabHeader() {
-		System.out.print("   0 1 2 3 4 5 6 7   <- X axis\n");
-		System.out.print("  +----------------+\n");
-	}
-	
-	public static void tabFooter() {
-		System.out.print("  +----------------+\n");
-		System.out.print("   0 1 2 3 4 5 6 7  \n");
-	}
-	
-	public static void printTab(int[][] m) {
-		for(int i = 0; i < 8; i++) {
-			// go through each matrix row
-			System.out.print(i + " |");
-			
-			// go through each cell (column with fixed row)
-			for(int j = 0; j < 8; j++) {
-				if (m[i][j] == 1 || m[i][j] == 2) { 
-					System.out.print(Integer.toString(m[i][j])); 
-				} else {
-					System.out.print(" ");
-				}
-				
-				System.out.print(" ");
-			}
-			
-			System.out.print("|\n");
-		}
-	}
 	
 	void changePlayer() {
 		if (player == 1) {
